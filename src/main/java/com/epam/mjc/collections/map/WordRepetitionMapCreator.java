9c64a70b863987a2d8a2e5638de5bd5b24446cc2
otabekJaval <112159831+otabekJaval@users.noreply.github.com>
2022-10-14 08:01:00 +0500

    Update WordRepetitionMapCreator.java

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
+
+        if(sentence.length()==0)return new HashMap<>();
+
+        sentence=sentence.replaceAll("[.,]","");
+        String[] s = sentence.split(" ");
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String s1 : s) {
+            map.merge(s1.toLowerCase(), 1, Integer::sum);
+        }
+
+        return map;
     }
 }
